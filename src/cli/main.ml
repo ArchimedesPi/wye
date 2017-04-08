8dfe134c40e8030704db56abf6a5729653b3b6b8
Liam <liam@cpan.org>
2017-04-08 15:12:12 -0500

    main.ml: Lex from a file or stdin with position tracking

--- src/cli/main.ml
+++ src/cli/main.ml
@@ -1,23 +1,36 @@
 open Core.Std
 open Lexing
 open Parser
+open Util
 
-let program = "print $ 2 != 4.";;
+let tokenize_with_error buf =
+  try (Ok (Lexer.tokenize buf)) with
+  | Lexer.Lex_error e -> Error e;;
 
-using discord as d.
-
-
+let print_error e buf =
+  Printf.printf "\n----\n%s: %s\n" (dump_position buf) e
 
 let rec lex buf =
-  let tok = try Lexer.tokenize buf with
-  | x -> print_endline (Exn.to_string x) in
+  let tok = tokenize_with_error buf in
   match tok with
-  | EOF -> ()
-  | t -> (
-    print_endline (dump_token t);
+  | Error e -> print_error e buf;
+  | Ok EOF -> print_endline "eof";
+  | Ok t -> (
+    print_string (dump_token t ^ "; ");
     lex buf
   )
   ;;
 
-let lexbuf = Lexing.from_string program in
-lex lexbuf
+let progin =
+  if Array.length Sys.argv > 1
+  then open_in Sys.argv.(1)
+  else stdin in
+let file_name = if progin = stdin then "stdin" else Sys.argv.(1) in
+let lexbuf = Lexing.from_channel progin in
+  lexbuf.Lexing.lex_curr_p <- {
+    Lexing.pos_fname = file_name;
+    pos_lnum = 1;
+    pos_bol = 0;
+    pos_cnum = 0; 
+  };
+  lex lexbuf
