5f6ed12679fc7b3259391ac784595278b85a3ac2
Liam <liam@cpan.org>
2017-04-25 16:51:22 -0500

    AST: add Message, Call, Func/VarBind, Using, Prototype, qualifier

--- src/wye/ast.ml
+++ src/wye/ast.ml
@@ -11,21 +11,40 @@ type binary_operator =
   | LessThanEqual
 [@@deriving show];;
 
-
-(* function argument :: argname; typehint option *)
-type fun_arg = FunctionArgument of string * string option [@@deriving show];;
-(* function arguments :: [argument] *)
-type fun_args = FunctionArguments of fun_arg array [@@deriving show];;
-
-type ast = (* all the root ast types *)
-  (* variant for numeric literals *)
-  | Number of [`Int of int | `Float of float]
-  | Variable of string
-  | String of string
-  (* binary operation :: operator; lhs; rhs *)
-  | BinaryOp of binary_operator * ast * ast
-  (* functions *)
-  | FunctionDefinition of fun_args * ast
+type qualifier =
+  | Mut
+  | Const
 [@@deriving show];;
 
+(* function argument :: argname; typehint option *)
+type argdef = string * string option [@@deriving show];;
+type proto = Prototype of string * argdef list [@@deriving show];;
+
+(* module path :: [submodule] *)
+type modpath = string list [@@deriving show];;
+(* identifier :: name; module option *)
+type ident = string * modpath [@@deriving show];;
+
+type ast =
+(* root types *)
+  (* variant for numeric literals *)
+  | Number of [`Int of int | `Float of float]
+  | Variable of ident
+  | Message of ident
+  | String of string
+(**)
+  | Call of ident * ast list
+  | FuncBind of proto * ast list
+  | VarBind of ident * ast * qualifier option
+(* === operators === *)
+  (* binary operation :: operator; lhs; rhs *)
+  | BinaryOp of binary_operator * ast * ast
+(* === meta === *)
+  (* using :: module; as option *)
+  | Using of string * string option
+  | Useless
+[@@deriving show];;
+
+
+(* a program is a flat collection of ASTs *)
 type program = ast list [@@deriving show];;
